35ba21141cfcdd414645f62b33dc7667113c1889
Jordan Griveau <jgriveau@yahoo.fr>
2023-09-24 11:51:03 +0200

    test: :white_check_mark: add hythesis factories and test cases for normal items

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,3 @@
-"""Tests to ensure that the gilded rose requirements are met."""
-
-import pytest
 from hypothesis import strategies as st
 
 from gilded_rose import Item
@@ -9,33 +6,24 @@ normal_item_names = st.sampled_from(["normal", "item", "foo"])
 brie = st.just("Aged Brie")
 sulfuras_strategy = st.just("Sulfuras, Hand of Ragnaros")
 backstage_pass = st.just("Backstage passes to a TAFKAL80ETC concert")
-conjured = st.just("Conjured Mana Cake")
 
 normal_quality = st.integers(min_value=0, max_value=50)
 sulfuras_quality = st.just(80)
+
 sell_in = st.integers(min_value=-10, max_value=10)
 
 
-@pytest.fixture
-def normal_item():
-    return Item("normal", 5, 10)
+def normal_item_factory():
+    return st.builds(Item, normal_item_names, sell_in, normal_quality)
 
 
-@pytest.fixture
-def aged_brie_item():
-    return Item("Aged Brie", 5, 10)
+def aged_brie_factory():
+    return st.builds(Item, brie, sell_in, normal_quality)
 
 
-@pytest.fixture
-def sulfuras_item():
-    return Item("Sulfuras, Hand of Ragnaros", 5, 80)
+def sulfuras_factory():
+    return st.builds(Item, sulfuras_strategy, sell_in, sulfuras_quality)
 
 
-@pytest.fixture
-def backstage_pass_item():
-    return Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)
-
-
-@pytest.fixture
-def conjured_item():
-    return Item("Conjured Mana Cake", 5, 10)
+def backstage_pass_factory():
+    return st.builds(Item, backstage_pass, sell_in, normal_quality)
